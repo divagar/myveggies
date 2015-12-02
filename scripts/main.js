070db09eaa46b8fb6f4e76bdfb5921e32977b8d0
Div <m.divagar@gmail.com>
2015-12-03 00:51:53 +0530

    Fix all the mobile web app align issues.
    Signed-off-by: Div <m.divagar@gmail.com>

--- scripts/main.js
+++ scripts/main.js
@@ -36,21 +36,22 @@ function getContent(name) {
 
 function createMainContent(name, data) {
    	$(".container .row").append(
-	      	"<div class='col-xs-2 col-sm-2 col-md-2 col-lg-2'>"+
-	      		"<div class='veggies-header'>"+
+   			"<div class='col-xs-5 col-sm-3 col-md-3 col-lg-2'>"+
+   				"<div class='veggies-header'>"+
 	      			"<h3>"+data.name+"</h3>"+
-					"<hr/><h6>"+data.desc+"</h6>"+
+					"<h6>"+data.desc+"</h6>"+
+					"<div class='price' data-price='"+data.price+"'>. . .</div>"+
+					"<hr/>"+
 				"</div>"+
 				"<div class='veggies-content'>"+
 					"<img src='images/"+name+"/"+data.name+".png'>"+
-					"<span class='price' data-price='"+data.price+"'>...</span>"+
 				"</div>"+
 			"</div>");
 }
 
 function updatePrice() {
-	$('div.veggies-content span.price').appear();
-	$('div.veggies-content span.price').on('appear', function() {
+	$('div.veggies-header div.price').appear();
+	$('div.veggies-header div.price').on('appear', function() {
 		console.log($(this));
 		$(this)[0].innerText = "Rs. " + $(this).data('price');
     	$(this).off();
