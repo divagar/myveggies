e88df539b5f21a18aea43658c6a0739e8fa7c503
Div <m.divagar@gmail.com>
2016-11-01 00:48:40 +0530

    updated modules and rewriting the master layout - wip
    Signed-off-by: Div <m.divagar@gmail.com>

--- src/app/myveggies.component.ts
+++ src/app/myveggies.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare var jQuery: any;
+
 @Component({
   selector: 'myveggies-root',
   templateUrl: './myveggies.component.html',
--- src/app/myveggies.module.ts
+++ src/app/myveggies.module.ts
@@ -1,20 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { routing, myveggiesRoutingProviders } from './myveggies.routing';
+import { LoginComponent } from './login.component';
+import { DashboardComponent } from './dashboard.component';
+import { HomeComponent } from './home.component';
 import { MyVeggiesComponent } from './myveggies.component';
+
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyB63VgD0hJpvuOs4rfQYtI9XeB_UIYv2YA",
+  authDomain: "myveggies-6f267.firebaseapp.com",
+  databaseURL: "https://myveggies-6f267.firebaseio.com",
+  storageBucket: "myveggies-6f267.appspot.com"
+};
 
 @NgModule({
   declarations: [
-    MyVeggiesComponent
+    MyVeggiesComponent,
+    HomeComponent,
+    LoginComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    ReactiveFormsModule,
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [
+    myveggiesRoutingProviders,
+    HomeComponent,
+    Title
+  ],
   bootstrap: [MyVeggiesComponent]
 })
 export class MyVeggiesModule { }
