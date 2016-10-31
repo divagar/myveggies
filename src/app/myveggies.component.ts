41a581f24481f8f54f50aa55e0b86efe95260727
Div <m.divagar@gmail.com>
2016-10-31 13:04:24 +0530

    Basic angular flow is in place
    Signed-off-by: Div <m.divagar@gmail.com>

---
+++ src/app/myveggies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'myveggies-root',
+  templateUrl: './myveggies.component.html',
+  styleUrls: ['./myveggies.component.css']
+})
+export class MyVeggiesComponent {
+  
+}
--- src/app/myveggies.module.ts
+++ src/app/myveggies.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { MyVeggiesComponent } from './myveggies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    MyVeggiesComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +15,6 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MyVeggiesComponent]
 })
-export class AppModule { }
+export class MyVeggiesModule { }
